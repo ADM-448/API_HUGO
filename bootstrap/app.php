a0a13e92c82936a7ca5f3d6fc6fbd9a6e51ebf9b
ADM-448 <138620256+ADM-448@users.noreply.github.com>
2026-05-14 19:51:17 -0300

    Configurar CORS para aceitar qualquer origem

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Registra HandleCors globalmente (antes de tudo)
+        $middleware->prepend(HandleCors::class);
+
         $middleware->alias([
             'auth.token'   => \App\Http\Middleware\VerificarToken::class,
             'auth.sessao'  => \App\Http\Middleware\VerificarSessao::class,
